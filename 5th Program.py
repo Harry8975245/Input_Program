62c92a447a2a4b81732c14bced88a21c1149849a
Harry8975245 <pc128065@gmail.com>
2026-08-11 15:23:54 +0530

    Input_Marks_Dictionary_Program

---
+++ 5th Program.py
@@ -0,0 +1,11 @@
+marks = {} 
+
+x=int(input("Hindi: " ))
+marks.update({"Hindi" :x })
+x=int(input("Maths :"))
+marks.update({"Maths" :x})
+x=int(input("Eng :"))
+marks.update({"Eng" :x})
+print(marks)
+                            
+
